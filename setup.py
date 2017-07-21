a80985ca9ac202826a313cfd2f23835edcb8ac0f
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-07-21 13:31:12 +0200

    Requirements

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         'django_dsl',
     ],
     include_package_data=True,
-    install_requires=[],
+    install_requires=["ply>=3.10", "Django>=1.10"],
     license="MIT",
     zip_safe=False,
     keywords='django-dsl',
